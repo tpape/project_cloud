fe0624a31e69fb79d878aa0fc5f5139fe66bf95a
Thomas Pape <thomas@mcaroni.local>
2015-01-18 12:23:02 +0100

    Add URL to get number of request per parameter

--- wsgi.py
+++ wsgi.py
@@ -6,16 +6,19 @@ import sqlite3
 import time
 
 DATABASE = './bdd/voitures.db'
+ARGS = ['annee', 'marque', 'modele', 'cnit', 'mine', 'carb', 'cv', 'puiss', 'bv', 'urb', 'exurb', 'mixte', 'co2']
+
 
 application = Flask(__name__)
 client = MongoClient('mongodb://localhost:27017/')
 db = client['cloud_stats']
 collection = db['requests']
 
-
+@application.before_request
 def log_access(): 
     args = dict(request.args)
-    args.update(request.view_args)
+    if request.view_args != None:
+        args.update(request.view_args)
     date = time.time()
     request_stat = {"args":args, "ts":date}
     collection.insert(request_stat)
@@ -24,11 +27,8 @@ def log_access():
 def get_voitures():
     sql = 'SELECT * FROM voitures'
     check = False
-    args = ['annee', 'marque', 'modele', 'cnit', 'mine', 'carb', 'cv', 'puiss', 'bv', 'urb', 'exurb', 'mixte', 'co2']
-    #print(request.args.items())
     for key, val in request.args.items():
-        
-        if key not in args:
+        if key not in ARGS:
             abort(400)
         else:
             if request.args.get(key) != None:
@@ -39,7 +39,6 @@ def get_voitures():
                 sql += 'lower('+key + ') = \'' + request.args.get(key).lower() + '\''
                 check = True
     voitures = query_db(sql)
-    #print(sql)
     return jsonify({'voitures': voitures})
 
 
@@ -48,6 +47,20 @@ def get_voiture(voiture_id):
     voiture = query_db('select * from voitures where rowid="{0}"'.format(voiture_id))
     print(voiture)
     return jsonify(voiture[0])
+
+@application.route('/api/v1.0/stats/', defaults={'key':None})
+@application.route('/api/v1.0/stats/<key>/')
+def get_top(key):
+    if key == None:
+        values = {}
+        result = ""
+        for val in ARGS :
+            values[val] = collection.find({'args.{0}'.format(val):{'$exists' : 'True'}}).count()
+        for val in sorted(values, key=values.get, reverse=True):
+            result = result + "<br>" + val + " " + str(values[val])
+        return result
+    else :
+        return("TODO")
 
 def connect_db():
     return sqlite3.connect(DATABASE)
@@ -65,7 +78,6 @@ def close_connection(exception):
         db.close()
 
 def query_db(query, args=(), one=False):
-    log_access()
     db = get_db()
     cur = db.execute(query, args)
     rv = [dict((cur.description[idx][0], value)
